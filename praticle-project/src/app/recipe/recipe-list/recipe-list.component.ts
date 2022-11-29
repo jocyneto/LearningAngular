99ed5fbee54c7b3866aab11905e37088d1ac78ca
Jocy Gonçalves Neto <jocygento@gmail.com>
2022-11-29 08:27:21 -0300

    finalizando projeto pratico parte 01, ainda serão feito mais modificacoes no projeto pratico

--- praticle-project/src/app/recipe/recipe-list/recipe-list.component.ts
+++ praticle-project/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('Teste de titulo', 'Teste de descrição', 'https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2020/11/11/escondidinho-de-frigideira-768x512.jpg'),
+    new Recipe('Teste de titulo', 'Teste de descrição', 'https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2020/11/11/escondidinho-de-frigideira-768x512.jpg')
+  ];
+
+
+  getName(){
+    return this.recipes[0].name
+  }
+
+  getDescription(){
+    return this.recipes[0].description
+  }
+
+  getImgPath(){
+    return this.recipes[0].imagePath
+  }
+  
+  ngOnInit(): void {
+  }
+
+  
 
 }
--- praticle-project/src/app/shopping-list/shopping-list.component.ts
+++ praticle-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[] = [
+    new Ingredient('Pineapple', 19),
+    new Ingredient('Cheese', 2),
+  ];
+  
+  ngOnInit(): void {
+  }
 
 }
